1b74a032bff7dfdc583bef90b111ccaabc69c3ce
kisky3 <kisky3@126.com>
2021-02-21 13:47:27 +0900

    :+1: main js の更新

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueEllipseProgress from 'vue-ellipse-progress'
+
+Vue.use(VueEllipseProgress)
 
 Vue.config.productionTip = false
 
