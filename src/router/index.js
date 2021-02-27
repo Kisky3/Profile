3b53eee880781760436c01fa05d722710be858a3
kisky3 <kisky3@126.com>
2021-02-27 22:52:05 +0900

    :shirt: package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(Router)
-Vue.use(BootstrapVue)
+Vue.use(Router);
+Vue.use(BootstrapVue);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     }
   ]
-})
+});
